24f5fd9e7350b130f51b84e3ca4931b6a31eced7
tinabenita <tina1717rego@gmail.com>
2025-03-13 08:39:48 +0530

    task: Added notes for SwitchMap

--- src/app/components/switch-map/switch-map.component.ts
+++ src/app/components/switch-map/switch-map.component.ts
@@ -12,6 +12,15 @@ import { of, switchMap } from 'rxjs';
 export class SwitchMapComponent implements OnInit {
  
   ngOnInit(): void {
+
+    /*
+    Switching Behavior: When a new item is emitted by the source Observable, switchMap unsubscribes from the previous inner Observable and subscribes to the new one.
+
+    Order Not Preserved: Only the latest inner Observable's emissions are considered, and previous ones are discarded if a new one starts. whereas in MergeMap, multiple observables can be active at the same time (concurrent). ConcatMap is sequential and does not discard the previous inner observables. 
+
+    Use Case: When you want to ensure that only the latest Observable's emissions are processed, such as in search or autocomplete scenarios where only the latest user input matters.
+    */
+
       const switched = of(1, 2, 3).pipe(
         switchMap(x => of(x, x ** 2, x ** 3))
       );
