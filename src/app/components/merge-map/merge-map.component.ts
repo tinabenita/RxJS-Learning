e1b6530376cfbdff9fd32ddde3acae524e57f065
tinabenita <tina1717rego@gmail.com>
2025-03-13 08:36:20 +0530

    task: Added notes for MergeMap

--- src/app/components/merge-map/merge-map.component.ts
+++ src/app/components/merge-map/merge-map.component.ts
@@ -11,6 +11,15 @@ import { of, mergeMap, interval, map } from 'rxjs';
 export class MergeMapComponent implements OnInit{
 
   ngOnInit(): void {
+
+    /* 
+    Parallel Execution: Inner Observables are processed in parallel, meaning multiple Observables can be active at the same time.
+
+    Order Not Preserved: Emissions from the resulting Observables can be out of order based on the timing of their completion.
+
+    Use Case: When you need to handle tasks concurrently and don't care about the order of emissions.
+    */
+
       const letters = of('a', 'b', 'c');
       const result = letters.pipe(
         mergeMap(x => interval(1000).pipe(map(i => x + i)))
