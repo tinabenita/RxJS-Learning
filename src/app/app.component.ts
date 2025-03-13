640776576e77c84ce0fba6acb11ed9a02a3629cd
tinabenita <tina1717rego@gmail.com>
2025-03-13 09:29:56 +0530

    task: CombineLatest Operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { MergeMapComponent } from "./components/merge-map/merge-map.component";
 import { SwitchMergeDiffComponent } from "./components/switch-merge-diff/switch-merge-diff.component";
 import { ConcatMapComponent } from "./components/concat-map/concat-map.component";
 import { ForkJoinComponent } from "./components/fork-join/fork-join.component";
+import { CombineLatestComponent } from "./components/combine-latest/combine-latest.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SwitchMapComponent, MergeMapComponent, SwitchMergeDiffComponent, ConcatMapComponent, ForkJoinComponent],
+  imports: [RouterOutlet, SwitchMapComponent, MergeMapComponent, SwitchMergeDiffComponent, ConcatMapComponent, ForkJoinComponent, CombineLatestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/combine-latest/combine-latest.component.ts
+++ src/app/components/combine-latest/combine-latest.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { combineLatest, interval, take } from 'rxjs';
 
 @Component({
   selector: 'app-combine-latest',
@@ -7,6 +8,34 @@ import { Component } from '@angular/core';
   templateUrl: './combine-latest.component.html',
   styleUrl: './combine-latest.component.scss'
 })
-export class CombineLatestComponent {
+export class CombineLatestComponent implements OnInit{
 
+  /*
+  Purpose: combineLatest combines the latest values from multiple Observables and emits them as an array whenever any of the Observables emit a new value.
+
+  Execution: It emits whenever one of the input Observables emits a new value, with the emitted array containing the latest values from all input Observables.
+
+  Use Case: Useful for scenarios where you need to react to the latest values from multiple sources.
+
+  ---Difference between ForkJoin and CombineLatest:---
+  forkJoin:
+  Emits only once, after all Observables have completed.
+  Emits an array containing the last emitted value from each Observable.
+  Ideal for tasks that need to be completed before processing the results collectively.
+
+  combineLatest:
+  Emits whenever any of the Observables emit a new value.
+  Emits an array containing the latest values from all Observables.
+  Ideal for real-time scenarios where you need the most up-to-date values from multiple sources.
+  */
+
+  ngOnInit(): void {
+    const obs1$ = interval(1000).pipe(take(3)); // Emits 0, 1, 2 at 1-second intervals
+    const obs2$ = interval(1500).pipe(take(2)); // Emits 0, 1 at 1.5-second intervals
+    const obs3$ = interval(2000).pipe(take(2)); // Emits 0, 1 at 2-second intervals
+    
+    console.log('CombineLatest Demo');
+    combineLatest([obs1$, obs2$, obs3$]).subscribe(results => console.log(results));
+    // Output:  [0, 0, 0], [1, 0, 0], [1, 1, 0], [2, 1, 0], [2, 1, 1]
+  }
 }
