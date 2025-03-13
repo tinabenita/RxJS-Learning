f6d13403440d88b017a2e20ddd55858a1311d124
tinabenita <tina1717rego@gmail.com>
2025-03-13 09:16:25 +0530

    task: ForkJoin Operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { SwitchMapComponent } from "./components/switch-map/switch-map.component
 import { MergeMapComponent } from "./components/merge-map/merge-map.component";
 import { SwitchMergeDiffComponent } from "./components/switch-merge-diff/switch-merge-diff.component";
 import { ConcatMapComponent } from "./components/concat-map/concat-map.component";
+import { ForkJoinComponent } from "./components/fork-join/fork-join.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SwitchMapComponent, MergeMapComponent, SwitchMergeDiffComponent, ConcatMapComponent],
+  imports: [RouterOutlet, SwitchMapComponent, MergeMapComponent, SwitchMergeDiffComponent, ConcatMapComponent, ForkJoinComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/fork-join/fork-join.component.ts
+++ src/app/components/fork-join/fork-join.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { delay, forkJoin, interval, of, take } from 'rxjs';
 
 @Component({
   selector: 'app-fork-join',
@@ -7,6 +8,33 @@ import { Component } from '@angular/core';
   templateUrl: './fork-join.component.html',
   styleUrl: './fork-join.component.scss'
 })
-export class ForkJoinComponent {
+export class ForkJoinComponent implements OnInit {
 
+  /*
+  Purpose: forkJoin is used to wait for multiple Observables to complete and then combine their last emitted values into a single Observable.
+
+  Execution: It subscribes to each input Observable and waits for all of them to complete. Once all Observables are completed, it emits an array containing the last emitted value from each Observable.
+
+  Use Case: Ideal for scenarios where you need to wait for multiple tasks to finish and then process their results.
+  */
+
+  constructor() { }
+  ngOnInit() { 
+
+    const obs1$ = of('A').pipe(delay(1000));
+    const obs2$ = of('B').pipe(delay(2000));
+    const obs3$ = of('C').pipe(delay(3000));
+    console.log('ForkJoin Demo 1');
+    forkJoin([obs1$, obs2$, obs3$])
+    .subscribe(results => console.log(results));
+
+    console.log('ForkJoin Demo 2');
+
+    const obs4$ = interval(1000).pipe(take(4)); // Emits 0, 1, 2, 3
+    const obs5$ = interval(1500).pipe(take(3)); // Emits 0, 1, 2
+    const obs6$ = interval(2000).pipe(take(2)); // Emits 0, 1
+
+    forkJoin([obs4$, obs5$, obs6$])
+    .subscribe(results => console.log(results));
+  }
 }
